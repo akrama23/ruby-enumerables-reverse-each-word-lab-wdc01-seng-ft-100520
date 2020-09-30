5da6b27997ede9e99a6e11443fc65a456fcea7c0
akam alam <arundelfootball23@gmail.com>
2020-09-30 20:57:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(string)
-real_sentence = string(" ")
+real_sentence = string.split(" ")
 sentence1_reversed = []
 
 real_sentence.each do |string|
   sentence1_reversed << string.reverse 
 end 
-sentence1_reversed
+sentence1_reversed.join(" ")
  
 end
